03b8462bbf505016002f9611ea65b435bcdcdf2c
Alex Usenko <alexx3003@ya.ru>
2024-01-14 12:08:30 +0300

    add some exercises

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 console.log("Hello world!");
+// if("") {
+//     alert("привет!")
+// }
+
+    // п.2.10 Условное ветвление if и ? задание 2 
+/*let oficialName = prompt("Какое официальное название JavaScript?","");
+if(oficialName=="ECMAScript") {
+    alert("Верно!!!")
+} else {
+    alert("Не знаете? “ECMAScript”!")
+}
+*/
+    // п.2.10 Условное ветвление if и ? задание 3
+/*
+let number = prompt("Введите любое число","");
+if(number==0){
+    alert ("0");
+} else {
+    if(number>0){
+        alert("1");
+    } else 
+    {
+        if(number<0)
+        {
+            alert("-1");
+        }
+    }
+}  это мой вариант кода*/
+
+    //а это вариант кода из учебника:
+/*
+    let value = prompt('Введите число', 0);
+if (value > 0) {
+    alert( 1 );
+} else if(value < 0) {
+        alert( -1 );
+    } else {
+        alert( 0 );
+    }
+*/
+
+// п.2.10 Условное ветвление if и ? задание 4
+/*
+let result;
+result== (a + b < 4) ? 'Мало': 'Много';
+*/
+
+// п.2.10 Условное ветвление if и ? задание 5
+/*
+let message = (login == 'Сотрудник') ? 'Привет' : (login == 'Директор') ? 'Здравствуйте' : (login == '') ? 'Нет логина' : '' ;
+*/
